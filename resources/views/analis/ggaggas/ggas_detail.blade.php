3cb0e3f7339f9846b9549e0cf5dc7b87fa8006f3
Praja04 <rajadamang13@gmail.com>
2025-05-19 14:08:11 +0700

    memperbaiki detail GGa GGAS

--- resources/views/analis/ggaggas/ggas_detail.blade.php
+++ resources/views/analis/ggaggas/ggas_detail.blade.php
@@ -228,7 +228,7 @@
             submitBtn.prop('disabled', true).text('Menyimpan...');
 
             $.ajax({
-                url: '/ggaggas/ggas/update-ajax/' + selectedId,
+                url: "{{url('/ggaggas/ggas/update-ajax/')}}" + selectedId,
                 method: 'POST',
                 data: form.serialize(),
                 success: function(response) {
